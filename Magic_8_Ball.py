20ad24d4551d72ea366dc8b8707a5c85864110d1
cagBRT <50635229+cagBRT@users.noreply.github.com>
2019-05-15 09:36:11 -0700

    Update Magic_8_Ball.py
    Added a 9th answer to the program.

--- Magic_8_Ball.py
+++ Magic_8_Ball.py
@@ -7,7 +7,7 @@ ans = True
 while ans:
     question = raw_input("Ask the magic 8 ball a question: (press enter to quit) ")
     
-    answers = random.randint(1,8)
+    answers = random.randint(1,9)
     
     if question == "":
         sys.exit()
@@ -38,4 +38,7 @@ while ans:
         
     elif answers == 8:
         print "My sources say no"
+     
+    elif answers == 9:
+       print "You need to get a life!"
     
